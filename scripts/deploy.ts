3da0ed8250da67ac8157f0c1d80e4acb96d6965e
fbrcode <fabio.bressler@gmail.com>
2022-07-05 09:10:32 -0300

    fix verify argument parameter

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -38,7 +38,7 @@ const verify = async (contractAddress: string, args: any[]) => {
   try {
     await run("verify:verify", {
       address: contractAddress,
-      constructorArgs: args,
+      constructorArguments: args,
     });
   } catch (error: any) {
     if (error.message.includes("already verified")) {
